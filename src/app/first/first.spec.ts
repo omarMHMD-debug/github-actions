12d4e989afdce4b6241f55e3c12bb9b5382fa8e4
omarMHMD-debug <algburi89@gmail.com>
2025-08-29 11:58:10 -0400

    add-unit-test

---
+++ src/app/first/first.spec.ts
@@ -0,0 +1,13 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { First } from './first';
+
+describe('First', () => {
+
+  let x= new First()
+
+
+  it('should create', () => {
+    expect(x.add()).toBe(1)
+  });
+});
---
+++ src/app/first/first.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-first',
+  imports: [],
+  templateUrl: './first.html',
+  styleUrl: './first.css'
+})
+export class First {
+
+  add() {
+    return 1
+  }
+
+}
