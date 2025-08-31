f4a1046e197e139d4e4b6bf3a1bd37a3cf701654
omarMHMD-debug <algburi89@gmail.com>
2025-08-31 12:44:55 -0400

    first commit

---
+++ src/app/first/first.spec.ts
@@ -0,0 +1,14 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { First } from './first';
+
+describe('First', () => {
+
+  let x=new First()
+ 
+
+
+  it('should create', () => {
+    expect(x.add()).toBe(20)
+  });
+});
